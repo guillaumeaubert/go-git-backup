30a484ff50718829830591069051744079bdb710
Guillaume Aubert <aubertg@cpan.org>
2016-05-04 20:39:44 -0700

    Add documentation on what gitbackup does.

--- gitbackup.go
+++ gitbackup.go
@@ -1,4 +1,40 @@
-// Main package.
+// go-git-backup offers a convenient way to back up remote GitHub/BitBucket
+// users/organizations with a collection of repositories for each.
+//
+// You first need to define a configuration file, for example ~/gitbackup.yml,
+// with the following content:
+//
+//     backup_directory: /where/your/backups/will/be/stored
+//     targets:
+//       - name: github-guillaumeaubert
+//         source: github
+//         type: users
+//         entity: aubertg
+//         token: mysecrettoken
+//       - name: bitbucket-aubertg
+//         source: bitbucket
+//         type: users
+//         entity: aubertg
+//         password: mysecretpassword
+//
+// You can define as many targets as your config file as you would like. Each
+// target should have the following information:
+//   * name: an internal name, used as the top level directory in your backup
+//     directory to group all the repositories belonging to this target.
+//   * source: "github" or "bitbucket". Other sources are not yet supported.
+//   * type: "users" or "orgs", depending on what type of entity you are
+//     backing up.
+//   * entity: the name of the entity being backed up, either a username or an
+//     organization name.
+//   * token: for GitHub, generate a token that gives access to this user or
+//     organization.
+//   * password: BitBucket doesn't support tokens yet, so you will need to use
+//     your normal password.
+//
+// Usage:
+//
+//     gitbackup --config=~/gitbackup.yml
+//
 package main
 
 import(
