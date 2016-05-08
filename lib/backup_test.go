84ce44977429e4e6b657f74cbe1763d198634a79
Guillaume Aubert <aubertg@cpan.org>
2016-05-07 22:31:23 -0700

    Add tests for includeRepository.

---
+++ lib/backup_test.go
@@ -0,0 +1,58 @@
+package gitbackup
+
+import(
+	"testing"
+	"github.com/davecgh/go-spew/spew"
+)
+
+type testdata struct {
+	repositoryName string
+	target         Target
+	expectedResult bool
+}
+
+var tests = []testdata{
+	{
+		"reponame",
+		Target{},
+		true,
+	},
+	{
+		"reponame",
+		Target{ Skip: "^repo" },
+		false,
+	},
+	{
+		"reponame",
+		Target{ Skip: "^test" },
+		true,
+	},
+	{
+		"reponame",
+		Target{ Only: "^test"},
+		false,
+	},
+	{
+		"reponame",
+		Target{ Only: "^repo"},
+		true,
+	},
+}
+
+func TestIncludeRepository(t *testing.T) {
+	for _, test := range tests {
+		result := includeRepository(
+			test.repositoryName,
+			test.target,
+		)
+		if result != test.expectedResult {
+			t.Errorf(
+				"Repository: %s\nTarget: %s\nExpected: %t\nGot: %t",
+				test.repositoryName,
+				spew.Sdump(test.target),
+				test.expectedResult,
+				result,
+			)
+		}
+	}
+}
